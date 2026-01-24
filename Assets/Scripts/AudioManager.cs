20b6cf86153621b7911c0cb3eb19cdf0a573bd20
Yonatan Ninio <yonatan.n@proton.me>
2026-01-25 01:30:08 +0200

    added new guns SFX assets

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,16 +4,34 @@ public class AudioManager : MonoBehaviour
 {
     // This value will control the master volume for the entire game (range 0 to 1)
     public float masterVolume = 0.1f;//1.0f;
+    AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         SetMasterVolume(masterVolume);
     }
 
-    // A public method to be called by a UI slider or other scripts
     public void SetMasterVolume(float volume)
     {
         masterVolume = volume;
         AudioListener.volume = masterVolume;
+    }
+
+    public void Reload1911_1()
+    {
+        // slide back
+    }
+    public void Reload1911_2()
+    {
+        // drop mag
+    }
+    public void Reload1911_3()
+    {
+        // insert new mag
+    }
+    public void Reload1911_4()
+    {
+        // drop slide / cock slide
     }
 }
